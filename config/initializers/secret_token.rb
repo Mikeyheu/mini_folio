1b776e9b0dafce025828c646033d7478be5f60f4
Michael Heu <mheu@clickbooq.com>
2012-08-23 11:45:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniFolio::Application.config.secret_token = 'f502dabd87437d6ccefddffa1505624b90f6c56ce69ea1afc04ac70d9962587dc367c0d82d78d7e7110e4b27c1ca33fe145025bf6bbd6deedfccfdd77798068b'
